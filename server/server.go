80c6a2861849da9f4159439a4d93b1a34c7d931c
MacBookAirM2 <uuxia@macbookairm2.com>
2024-09-09 20:51:12 +0800

    2024-09-09 20:51:12 by uuxia

--- server/server.go
+++ server/server.go
@@ -3,11 +3,9 @@ package server
 import (
 	"github.com/gorilla/mux"
 	"github.com/xxl6097/go-glog/glog"
-	"github.com/xxl6097/go-http/cmd/app/static/assets"
 	"github.com/xxl6097/go-http/pkg/api"
 	"github.com/xxl6097/go-http/server/middle"
 	"github.com/xxl6097/go-http/server/route"
-	"github.com/xxl6097/go-http/server/util"
 	"net"
 	"net/http"
 	"time"
@@ -23,7 +21,6 @@ type Server struct {
 }
 
 func NewServer() *Server {
-	assets.Load("")
 	this := &Server{
 		router: mux.NewRouter(),
 	}
@@ -35,14 +32,6 @@ func (this *Server) initApi() {
 	//顺序，后面最先调用
 	this.router.Use(middle.EnableCors, middle.HandleOptions, middle.AuthMiddleware)
 	this.router.Use(mux.CORSMethodMiddleware(this.router))
-	route.RouterUtil.AddNoAuthPrefix("/")
-	route.RouterUtil.AddNoAuthPrefix("js")
-	route.RouterUtil.AddNoAuthPrefix("css")
-
-	// view
-	this.router.Handle("/favicon.ico", http.FileServer(assets.FileSystem)).Methods(http.MethodGet, http.MethodOptions)
-	this.router.PathPrefix("/").Handler(util.MakeHTTPGzipHandler(http.StripPrefix("/", http.FileServer(assets.FileSystem)))).Methods(http.MethodGet, http.MethodOptions)
-
 	api.GetApi().Setup(this.router)
 }
 
